acc9a89279398b47e214ad975c6aea6f63d63e9b
vagif4799 <quliyev_vaqif@icloud.com>
2020-02-28 09:24:15 +0400

    -- Function<> apply and andThen methods

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -5,16 +5,27 @@ import java.util.function.Function;
 public class _Function {
 
     public static void main(String[] args) {
+
         int increment = incrementbyOne(1);
         System.out.println(increment);
 
         int increment2 = incrementByOneFunction.apply(1);
         System.out.println(increment2);
 
+        int multiply = multiplyBy10Function.apply(increment2);
+        System.out.println(multiply);
+
+        Function<Integer, Integer> addBy1AndThenMultiplyBy10 = incrementByOneFunction.andThen(multiplyBy10Function);
+        Integer incrementAndMultiply = addBy1AndThenMultiplyBy10.apply(1);
+        System.out.println(incrementAndMultiply);
+
     }
 
     static Function<Integer, Integer> incrementByOneFunction =
             number -> number + 1;
+
+    static Function<Integer, Integer> multiplyBy10Function =
+            number -> number * 10;
 
     static int incrementbyOne(int number) {
         return number + 1;
